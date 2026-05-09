fce574ff8193ee80ed8d4663abd54b58e5196a32
fesily <fesil@foxmail.com>
2026-05-09 10:23:53 +0800

    Refactor incremental garbage collection tests for clarity and structure

--- test/lang/gcinc.lua
+++ test/lang/gcinc.lua
@@ -1,60 +1,69 @@
-print('testing incremental garbage collection')
-
 local jit = require"jit"
 
 jit.off()
 
-assert(collectgarbage("isrunning"))
-
-collectgarbage()
-
-local oldmode = collectgarbage("incremental")
-
-assert(collectgarbage("generational") == "incremental")
-assert(collectgarbage("generational") == "generational")
-assert(collectgarbage("incremental") == "generational")
-assert(collectgarbage("incremental") == "incremental")
-
-local function gcinfo()
-  return collectgarbage("count") * 1024
-end
-
-local function dosteps(stepsize)
+do --- mode_switching
+  assert(collectgarbage("isrunning"))
   collectgarbage()
-  local objects = {}
-  for i = 1, 100 do
-    objects[i] = {{}}
-    local scratch = {}
-  end
-  local before = gcinfo()
-  local steps = 0
-  repeat
-    steps = steps + 1
-  until collectgarbage("step", stepsize)
-  assert(gcinfo() < before)
-  return steps
+  local oldmode = collectgarbage("incremental")
+  assert(collectgarbage("generational") == "incremental")
+  assert(collectgarbage("generational") == "generational")
+  assert(collectgarbage("incremental") == "generational")
+  assert(collectgarbage("incremental") == "incremental")
+  collectgarbage(oldmode)
 end
 
-collectgarbage("stop")
-assert(dosteps(10) < dosteps(2))
-collectgarbage("restart")
-assert(collectgarbage("isrunning"))
+do --- step_size
+  local function gcinfo()
+    return collectgarbage("count") * 1024
+  end
 
-do
+  local function dosteps(stepsize)
+    collectgarbage()
+    local objects = {}
+    for i = 1, 100 do
+      objects[i] = {{}}
+      local scratch = {}
+    end
+    local before = gcinfo()
+    local steps = 0
+    repeat
+      steps = steps + 1
+    until collectgarbage("step", stepsize)
+    assert(gcinfo() < before)
+    return steps
+  end
+
+  local oldmode = collectgarbage("incremental")
+  collectgarbage("stop")
+  assert(dosteps(10) < dosteps(2))
+  collectgarbage("restart")
+  assert(collectgarbage("isrunning"))
+  collectgarbage(oldmode)
+end
+
+do --- gc_stopped_alloc
+  local function gcinfo()
+    return collectgarbage("count") * 1024
+  end
+
+  local oldmode = collectgarbage("incremental")
   collectgarbage()
   collectgarbage("stop")
   collectgarbage("step")
   local before = gcinfo()
+  local holder = {}
   repeat
     for i = 1, 1000 do
-      _G.__gcinc_tmp = {}
+      holder[#holder + 1] = {}
     end
   until gcinfo() > 2 * before
   collectgarbage("restart")
-  _G.__gcinc_tmp = nil
+  collectgarbage(oldmode)
 end
 
-do
+do --- weak_table_collection
+  local oldmode = collectgarbage("incremental")
   local weak = setmetatable({}, {__mode = "kv"})
   local key, value = {}, {}
   weak[key] = value
@@ -62,8 +71,5 @@ do
   value = nil
   collectgarbage()
   assert(next(weak) == nil)
+  collectgarbage(oldmode)
 end
-
-collectgarbage(oldmode)
-
-print('OK')
